0d7a7bcf36e5acfa7b60d3628f33399e0f370f23
profesor <profesor>
2017-10-04 21:54:44 -0300

    Mas ejercicios de asincronismo

--- ServidorPHP/partes/texto.php
+++ ServidorPHP/partes/texto.php
@@ -3,4 +3,5 @@
 	<p><strong>3-</strong> Mostrar en el principal si el numero random generado es positivo o negativo </p>
 	<p><strong>4-</strong> Pasar los parametros por GET para el maximo y el minimo del random </p>
 	<p><strong>5-</strong> Pasar los parametros por POST para el maximo y el minimo del random </p>
+	<p><strong>6-</strong> Si el numero random es par  guardarlo con "GuardarNumero" de js </p>
 	
--- js/funcionesAjax.js
+++ js/funcionesAjax.js
@@ -31,51 +31,7 @@ function MostrarSinParametros()
 	});
 
 }
-function MostrarRetornandoPromesa1(queMostrar)
-{
-	$("#informe").html("<p> Conectando con el servidor...</p><img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
-	$("#error").html("");
-	return $.ajax({
-		url:"./ServidorPHP/nexo.php",
-		type:"post",
-		data:{queHacer:queMostrar}
-	})
-	.then(function(retorno){
-		//$("#principal").html(retorno);
-		//$("#informe").html("Correcto!!!");
-		console.log("volvio MostrarRetornandoPromesa1");
-		return retorno;
-	})
-	.catch(function(retorno){
-		//$("#principal").html(":(");
-		//$("#informe").html(retorno.responseText);
-		return retorno.responseText;
-	});
-}
-function MostrarRetornandoPromesa2(queMostrar)
-{
-	$("#informe").html("<p> Conectando con el servidor...</p><img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
-	$("#error").html("");
-	var ObjetoAjax= $.ajax({
-		url:"./ServidorPHP/nexo.php",
-		type:"post",
-		data:{queHacer:queMostrar}
-	})
-	/*
-	.then(function(retorno){
-		//$("#principal").html(retorno);
-		//$("#informe").html("Correcto!!!");
 
-		return retorno;
-	})
-	.catch(function(retorno){
-		//$("#principal").html(":(");
-		//$("#informe").html(retorno.responseText);
-		return retorno.responseText;
-	});
-	*/
-	return ObjetoAjax;
-}
 function MostrarSinRetornarPromesa(queMostrar)
 {
 	$("#informe").html("<img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
@@ -203,6 +159,75 @@ function MostarRandomlimitadoPost()
 	var funcionAjax=$.ajax({
 		url:"./ServidorPHP/php/RetornarRandomlimitadoPost.php",
 		type:"post"
+	})
+	.then(function(retorno){
+		console.info(retorno);
+		$("#principal").html(retorno);
+		$("#informe").html("Correcto MostarRandomlimitadoPost");	
+	})
+	.catch(function(retorno){
+		console.log(retorno);
+		$("#informe").html("Retorno con error");
+		$("#principal").html("Error :" +retorno.statusText +"<br>"+ retorno.status);	
+		$("#error").html(retorno.responseText);	
+	});
+
+}
+
+function MostrarRetornandoPromesa1(queMostrar)
+{
+	$("#informe").html("<p> Conectando con el servidor...</p><img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
+	$("#error").html("");
+	return $.ajax({
+		url:"./ServidorPHP/nexo.php",
+		type:"post",
+		data:{queHacer:queMostrar}
+	})
+	.then(function(retorno){
+		//$("#principal").html(retorno);
+		//$("#informe").html("Correcto!!!");
+		console.log("volvio MostrarRetornandoPromesa1");
+		return retorno;
+	})
+	.catch(function(retorno){
+		//$("#principal").html(":(");
+		//$("#informe").html(retorno.responseText);
+		return retorno.responseText;
+	});
+}
+function MostrarRetornandoPromesa2(queMostrar)
+{
+	$("#informe").html("<p> Conectando con el servidor...</p><img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
+	$("#error").html("");
+	var ObjetoAjax= $.ajax({
+		url:"./ServidorPHP/nexo.php",
+		type:"post",
+		data:{queHacer:queMostrar}
+	})
+	/*
+	.then(function(retorno){
+		//$("#principal").html(retorno);
+		//$("#informe").html("Correcto!!!");
+
+		return retorno;
+	})
+	.catch(function(retorno){
+		//$("#principal").html(":(");
+		//$("#informe").html(retorno.responseText);
+		return retorno.responseText;
+	});
+	*/
+	return ObjetoAjax;
+}
+function GuardarNumero()
+{
+
+	$("#informe").html("<img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
+	$("#error").html("");
+	var funcionAjax=$.ajax({
+		url:"./ServidorPHP/php/GuardarNumero.php",
+		type:"post",
+		data:{numero:10}
 	})
 	.then(function(retorno){
 		console.info(retorno);
