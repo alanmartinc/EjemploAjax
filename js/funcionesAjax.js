5b25055a14783a11554ad7fcfc9bd0273c61f586
octavio <el.octavio.villegas@gmail.com>
2017-10-05 11:08:54 -0300

    cambio de archivos y menu

--- js/funcionesAjax.js
+++ js/funcionesAjax.js
@@ -74,105 +74,7 @@ function MostarLogin()
 
 }
 
-function MostarPaises()
-{
 
-	$("#informe").html("<img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
-	$("#error").html("");
-	var funcionAjax=$.ajax({
-		url:"https://restcountries.eu/rest/v2/region/americas",
-	
-	})
-	.then(function(retorno){
-		console.info(retorno);
-		$("#principal").html(retorno);
-		$("#informe").html("Correcto MostarPaises ");	
-	})
-	.catch(function(retorno){
-		$("#botonesABM").html(":(");
-		$("#informe").html(retorno.responseText);	
-	});
-
-}
-
-function MostarPais()
-{
-
-	$("#informe").html("<img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
-	$("#error").html("");
-	var funcionAjax=$.ajax({
-		url:"https://restcountries.eu/rest/v2/name/argentina",
-	
-	})
-	.then(function(retorno){
-		console.info(retorno);
-		$("#principal").html(retorno);
-		$("#informe").html("Correcto MostarPais");	
-	})
-	.catch(function(retorno){
-		$("#botonesABM").html(":(");
-		$("#informe").html(retorno.responseText);	
-	});
-
-}
-
-
-function MostarRandom()
-{
-
-	$("#informe").html("<img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
-	$("#error").html("");
-	var funcionAjax=$.ajax({
-		url:"./ServidorPHP/php/RetornarRandom.php"
-	
-	})
-	$("#informe").html("");
-
-}
-
-function MostarRandomlimitadoGet()
-{
-
-	$("#informe").html("<img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
-	$("#error").html("");
-	var funcionAjax=$.ajax({
-		url:"./ServidorPHP/php/RetornarRandomlimitadoGet.php",
-		type:"get"
-	})
-	.then(function(retorno){
-		console.info(retorno);
-		$("#principal").html(retorno);
-		$("#informe").html("Correcto MostarRandomlimitadoGet");	
-	})
-	.catch(function(retorno){
-		$("#principal").html("Error:"+retorno.statusText +"<br>"+ retorno.status);
-		$("#informe").html("Retorno con error");
-		$("#error").html(retorno.responseText);	
-	});
-
-}
-function MostarRandomlimitadoPost()
-{
-
-	$("#informe").html("<img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
-	$("#error").html("");
-	var funcionAjax=$.ajax({
-		url:"./ServidorPHP/php/RetornarRandomlimitadoPost.php",
-		type:"post"
-	})
-	.then(function(retorno){
-		console.info(retorno);
-		$("#principal").html(retorno);
-		$("#informe").html("Correcto MostarRandomlimitadoPost");	
-	})
-	.catch(function(retorno){
-		console.log(retorno);
-		$("#informe").html("Retorno con error");
-		$("#principal").html("Error :" +retorno.statusText +"<br>"+ retorno.status);	
-		$("#error").html(retorno.responseText);	
-	});
-
-}
 
 function MostrarRetornandoPromesa1(queMostrar)
 {
@@ -219,26 +121,31 @@ function MostrarRetornandoPromesa2(queMostrar)
 	*/
 	return ObjetoAjax;
 }
-function GuardarNumero()
+
+
+
+
+function TraerBotones(numero)
 {
 
-	$("#informe").html("<img src='imagenes/ajax-loader.gif' style='width: 30px;'/>");
-	$("#error").html("");
-	var funcionAjax=$.ajax({
-		url:"./ServidorPHP/php/GuardarNumero.php",
-		type:"post",
-		data:{numero:10}
-	})
-	.then(function(retorno){
-		console.info(retorno);
-		$("#principal").html(retorno);
-		$("#informe").html("Correcto MostarRandomlimitadoPost");	
-	})
-	.catch(function(retorno){
-		console.log(retorno);
-		$("#informe").html("Retorno con error");
-		$("#principal").html("Error :" +retorno.statusText +"<br>"+ retorno.status);	
-		$("#error").html(retorno.responseText);	
-	});
+	switch(numero)
+	{
+		
+		case 1:
+		$("#main-nav").html('<li><a onclick="MostrarError()" class="btn">Ajax<br> Error!!!</a></li><li><a onclick="MostrarSinParametros()" class="btn">Ajax<br>S/Parametros</a> </li> <li><a onclick="MostrarSinRetornarPromesa(\'foto\')" class="btn">Ajax Foto<br>(parametros<br> y POST) </a> </li><li><a onclick="MostrarSinRetornarPromesa(\'video\')" class="btn">Ajax Video<br>(parametros<br> y POST)</a> </li><li><a onclick="TraerBotones(2)" class="btn btn-success">Mas -> </a> </li>');
+		break;
+		case 2:
+		$("#main-nav").html('<li><a onclick="MostarPais()" class="btn">Ajax<br> Mostrar<br>Argentina</a></li> <li><a onclick="MostarPaises()" class="btn">Ajax<br> Mostrar<br>paises</a></li>		<li><a onclick="TraerBotones(3)" class="btn btn-success"> Ejercicio asincrono-> </a> </li>');
+		break;
+		case 3:
+		$("#main-nav").html('<li><a onclick="MostarRandom()" class="btn">Ajax<br>Random</a></li>				<li><a onclick="GuardarNumero()" class="btn">Ajax<br>Guardar</a></li>					<li><a onclick="MostarRandomlimitadoGet()" class="btn">Ajax<br> Random <br> limitado <br>GET</a></li>								<li><a onclick="MostarRandomlimitadoPost()" class="btn">Ajax<br> Random <br>limitado<br>POST</a></li>		 </li><li><a onclick="TraerBotones(4)" class="btn btn-success">ABM Con Login</a> </li>');
+		break;
+		case 4:
+		$("#main-nav").html('<li><a onclick="MostrarSinRetornarPromesa(\'MostrarGrilla\')" class="btn">Mostrar <br>Grilla<br>-Sesión-</a> </li>							<li><a onclick="MostarLogin()"  class="btn btn-primary"id="BotonLogin" >Login<br>-Sesión-</a> </li>	 </li><li><a onclick="TraerBotones(1)" class="btn btn-success">inicio -> </a> </li>');
+		break;
+
+	}
+	
+	
 
 }
